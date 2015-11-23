c03a64adccfb2aba324846cfa415c9f6766ea85b
vmartinezf <vanessa_1895@msn.com>
2015-11-23 10:36:30 +0100

    otras cosas

--- client.py
+++ client.py
@@ -10,17 +10,27 @@ import sys
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-METHOD = sys.argv[1]
-line_parameters = sys.argv[2]
-LOGIN = line_parameters.split('@')[0] + '@'
-linea_2parameters = line_parameters.split('@')[1]
-IPSERVER = line_2parameters.split(':')[0]
-PORT = int(line_2parameters.split(':')[1])
+if len(sys.argv) != 3: 
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
+try:
+    METHOD = sys.argv[1]
+    line_parameters = sys.argv[2]
+    LOGIN = line_parameters.split('@')[0] + '@'
+    linea_2parameters = line_parameters.split('@')[1]
+    IPSERVER = line_2parameters.split(':')[0]
+    PORT = int(line_2parameters.split(':')[1])
+except:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 if PORT < 1024:
     sys.exit("ERROR: PORT IS INCORRECT")
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+Line_sip = " sip:" + LOGIN + IP + " SIP/2.0\r\n"
+if METHOD == 'INVITE':
+    LINE = "INVITE" + Line_sip
+elif METHOD == "BYE":
+    LINE = "BYE" + Line_sip
+    
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
