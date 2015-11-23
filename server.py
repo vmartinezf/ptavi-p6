41a1f997e9f56fb2a4a46fb65c75fda4d60ba492
vmartinezf <vanessa_1895@msn.com>
2015-11-23 10:56:21 +0100

    nuevo server

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+
+
+def information (line_decod):
+    sip = line_decod.split()[1]
+    LOGIN = sip.split('sip:')[1]
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -18,7 +24,8 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+            line_decod = line.decode('utf-8')
+            print("El cliente nos manda " + line_decod)
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
@@ -26,6 +33,14 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    try:
+        IP = sys.argv[1]
+        PORT = int(sys.argv[2])
+        if PORT < 1024:
+            sys.exit("Error: port is invalid")
+        FICHAUDIO = sys.argv[3]
+    except:
+        sys.exit("Usage: python server.py IP port audio_file")
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
